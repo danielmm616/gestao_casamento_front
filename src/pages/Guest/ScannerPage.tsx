3c7e316f312030fd079fb85df6005445dcc6e406
Daniel Morais <ddan.mm.16@gmail.com>
2025-08-07 18:16:28 -0300

    Set document title for ScannerPage component

--- src/pages/Guest/ScannerPage.tsx
+++ src/pages/Guest/ScannerPage.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import { QRCodeReaderJSQR } from '../../Components/QrCodeScanner';
 
 export default function ScannerPage() {
+  useEffect(() => {
+    document.title = 'D&R | Scanner de QR Code';
+  }, []);
+
   return (
     <div>
       <QRCodeReaderJSQR />
